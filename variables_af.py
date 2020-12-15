ffa331901a3956472c0d62d922ffef2144369b60
roelsmeets <r.smeets@let.ru.nl>
2020-12-15 11:20:06 +0100

    File with pathname variables

---
+++ variables_af.py
@@ -0,0 +1,19 @@
+# #!/usr/bin/env python
+
+# # -*- coding: utf-8 -*-
+
+
+bookpath = {}
+bookpath['1'] = '/Users/roelsmeets/desktop/af_corpora/af_corpus_1stpers_clean'
+bookpath['2'] = '/Users/roelsmeets/desktop/af_corpora/af_corpus_3rdpers_clean'
+bookpath['3'] = '/Users/roelsmeets/desktop/af_corpora/af_corpus_multi_clean_annotated' 
+bookpath['4'] = '/Users/roelsmeets/desktop/af_corpora/af_corpus_other_clean'
+
+csvpath = '/Users/roelsmeets/desktop/actual_fictions/actual-fictions'
+csvfiles = {}
+csvfiles['books'] = csvpath + '/BOOKS_AF_test.csv'
+csvfiles['nodes'] = csvpath + '/NODES__AF_test.csv'
+csvfiles['edges'] = csvpath + '/EDGES__AF_test.csv'
+csvfiles['names'] = csvpath + '/NAMES__AF_test.csv'
+csvfiles['rankings'] = csvpath + '/character-rankings.csv'
+
